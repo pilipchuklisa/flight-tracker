cb224ea770c9361f3597f3a3e00744ef80124157
Lisa <pilipchchlisa@gmail.com>
2025-02-11 17:09:27 +0300

    Initial commit

---
+++ src/main/java/com/example/flight_tracker/FlightTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.flight_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTrackerApplication.class, args);
+	}
+
+}
